e76a52c0287436ec4095f0e06ebf571000b5a632
ifjso <ojsboy@hotmail.com>
2022-01-10 09:32:54 +0900

    feat: 재고 및 장바구니 view 추가

--- ch2-reactive/src/main/java/com/js/hackingspringboot/reactive/ch2/HomeController.java
+++ ch2-reactive/src/main/java/com/js/hackingspringboot/reactive/ch2/HomeController.java
@@ -2,13 +2,26 @@ package com.js.hackingspringboot.reactive.ch2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
 
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
+
+    public HomeController(ItemRepository itemRepository, CartRepository cartRepository) {
+        this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
+    }
+
     @GetMapping
-    Mono<String> home() {
-        return Mono.just("home");
+    Mono<Rendering> home() {
+        return Mono.just(Rendering.view("home.html")
+            .modelAttribute("items", itemRepository.findAll())
+            .modelAttribute("cart", cartRepository.findById("My Cart")
+                .defaultIfEmpty(new Cart("My Cart")))
+            .build());
     }
 }

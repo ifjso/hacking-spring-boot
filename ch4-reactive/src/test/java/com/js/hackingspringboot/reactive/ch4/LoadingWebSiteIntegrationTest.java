706b5262a7098676f35d1f40592fb58d4199144e
ifjso <ojsboy@hotmail.com>
2022-01-20 23:46:47 +0900

    test: 종단 간 테스트 추가

---
+++ ch4-reactive/src/test/java/com/js/hackingspringboot/reactive/ch4/LoadingWebSiteIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.js.hackingspringboot.reactive.ch4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class LoadingWebSiteIntegrationTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void test() {
+        client.get().uri("/").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.TEXT_HTML)
+                .expectBody(String.class)
+                .consumeWith(exchangeResult -> {
+                    assertThat(exchangeResult.getResponseBody()).contains("action=\"/add");
+                });
+    }
+}

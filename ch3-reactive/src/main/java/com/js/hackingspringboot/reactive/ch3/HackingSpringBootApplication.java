bdfe6fb42c4ac935e7ab0c7c177ee14de7fe7628
ifjso <ojsboy@hotmail.com>
2022-01-20 19:46:11 +0900

    feat: blocking 검출을 위해 BlockHound 추가

--- ch3-reactive/src/main/java/com/js/hackingspringboot/reactive/ch3/HackingSpringBootApplication.java
+++ ch3-reactive/src/main/java/com/js/hackingspringboot/reactive/ch3/HackingSpringBootApplication.java
@@ -2,12 +2,17 @@ package com.js.hackingspringboot.reactive.ch3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class HackingSpringBootApplication {
 
 	public static void main(String[] args) {
+		BlockHound.builder()
+				.allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "initialize")
+				.install();
+
 		SpringApplication.run(HackingSpringBootApplication.class, args);
 	}
-
 }

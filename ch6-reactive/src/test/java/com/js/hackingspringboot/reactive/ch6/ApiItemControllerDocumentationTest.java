72d041f49a24fc1376c7de96429741ce8457481a
ifjso <ojsboy@hotmail.com>
2022-02-04 17:59:39 +0900

    feat: 아이템 한 건 조회 및 수정 api docs 테스트 추가

--- ch6-reactive/src/test/java/com/js/hackingspringboot/reactive/ch6/ApiItemControllerDocumentationTest.java
+++ ch6-reactive/src/test/java/com/js/hackingspringboot/reactive/ch6/ApiItemControllerDocumentationTest.java
@@ -1,5 +1,12 @@
 package com.js.hackingspringboot.reactive.ch6;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -8,14 +15,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.maskLinks;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 
 @WebFluxTest(controllers = ApiItemController.class)
 @AutoConfigureRestDocs
@@ -64,5 +63,41 @@ class ApiItemControllerDocumentationTest {
                         "post-new-item",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    void findOneItem() {
+        given(itemRepository.findById("item-1")).willReturn(
+                Mono.just(Item.builder()
+                        .id("item-1")
+                        .name("Alf alarm clock")
+                        .description("nothing I really need")
+                        .price(19.99d)
+                        .build()));
+
+        webTestClient.get().uri("/api/items/item-1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(document("findOne", preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    void updateItem() {
+        Item item = Item.builder()
+                .id("1")
+                .name("Alf alarm clock")
+                .description("updated")
+                .price(19.99d)
+                .build();
+        given(itemRepository.save(any())).willReturn(
+                Mono.just(item));
+
+        webTestClient.put().uri("/api/items/1")
+                .bodyValue(item)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(document("update-item", preprocessResponse(prettyPrint())));
     }
 }

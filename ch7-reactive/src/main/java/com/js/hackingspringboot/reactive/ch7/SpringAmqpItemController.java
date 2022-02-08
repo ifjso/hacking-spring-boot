f92bacbc7848e5392b6d0404f1d626a50a6f31f9
ifjso <ojsboy@hotmail.com>
2022-02-08 23:57:22 +0900

    feat: item 등록 시 amqp를 이용하여 메세지를 보내는 api 추가

---
+++ ch7-reactive/src/main/java/com/js/hackingspringboot/reactive/ch7/SpringAmqpItemController.java
@@ -0,0 +1,32 @@
+package com.js.hackingspringboot.reactive.ch7;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.net.URI;
+
+@Slf4j
+@RestController
+public class SpringAmqpItemController {
+
+    private final AmqpTemplate template;
+
+    public SpringAmqpItemController(AmqpTemplate template) {
+        this.template = template;
+    }
+
+    @PostMapping("/items")
+    Mono<ResponseEntity<?>> addNewItemUsingSpringAmqp(@RequestBody Mono<Item> item) {
+        return item.subscribeOn(Schedulers.boundedElastic())
+                .flatMap(content -> Mono.fromCallable(() -> {
+                        template.convertAndSend("hacking-spring-boot", "new-items-spring-amqp", content);
+                        return ResponseEntity.created(URI.create("/items")).build();
+                    }));
+    }
+}

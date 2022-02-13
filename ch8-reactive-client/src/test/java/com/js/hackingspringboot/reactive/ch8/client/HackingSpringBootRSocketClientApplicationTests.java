5742005e01cdb188e53f875b1d4f93e830b6b2d9
ifjso <ojsboy@hotmail.com>
2022-02-13 23:02:21 +0900

    feat: chapter8 rsocket client 추가

---
+++ ch8-reactive-client/src/test/java/com/js/hackingspringboot/reactive/ch8/client/HackingSpringBootRSocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.hackingspringboot.reactive.ch8.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingSpringBootRSocketClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

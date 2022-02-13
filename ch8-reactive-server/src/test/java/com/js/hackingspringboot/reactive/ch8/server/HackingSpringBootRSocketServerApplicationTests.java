c833257e4286af2292856f50d9289426b8b1229c
ifjso <ojsboy@hotmail.com>
2022-02-13 21:47:45 +0900

    feat: chapter8 rsocket server 추가

---
+++ ch8-reactive-server/src/test/java/com/js/hackingspringboot/reactive/ch8/server/HackingSpringBootRSocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.hackingspringboot.reactive.ch8.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingSpringBootRSocketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
